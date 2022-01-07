885717d12b569fad366d8dc53b55df83050591ce
Lakshay Arora <72307080+LakshayArora05@users.noreply.github.com>
2022-01-08 03:42:48 +0530

    adduser,java
    addition by asking for values from the user

---
+++ adduser.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ class Inputfunctions {
+  public static void main(String[] args) {
+   Scanner read = new Scanner(System.in);
+   System.out.println("Enter Two Numbers:");
+   int a,b,sum;
+   a=read.nextInt();
+   b=read.nextInt();
+   sum=a+b;
+   System.out.println("sum is: " +sum);
+   }
+   }
